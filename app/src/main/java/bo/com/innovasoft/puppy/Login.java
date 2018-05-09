e0a1379c605077503ac60f16698120b686c55ecf
Jhon Douglas <jhondouglas.87@gmail.com>
2018-05-09 22:07:21 +0100

    falta colocar el codigo a toda la pantalla de inicio de sesion ya esta el activity de para crear el usuario

--- app/src/main/java/bo/com/innovasoft/puppy/Login.java
+++ app/src/main/java/bo/com/innovasoft/puppy/Login.java
@@ -1,14 +1,30 @@
 package bo.com.innovasoft.puppy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        siguiente=(Button)findViewById(R.id.btncrearcuenta);
+        siguiente.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent siguiente=new Intent(Login.this, CrearCuenta.class);
+                startActivity(siguiente);
+                finish();
+            }
+        });
+
     }
 }
